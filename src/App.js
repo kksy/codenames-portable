61c9d90784b9e9dbff234bfcf2b6413046534293
Kylie Sy <kylksy@gmail.com>
2019-04-02 21:58:27 +1100

    Choose player type

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AgentScreen from './AgentScreen';
 import SpymasterScreen from './SpymasterScreen';
+import './App.css';
 
 const App = () => {
-  return <div>
-    <AgentScreen />
-    <SpymasterScreen />
-  </div>
+  const [user, setUser] = useState(null);
+  const screen = {
+    agent: <AgentScreen />,
+    spymaster: <SpymasterScreen />,
+  }
+  const ChoosePlayerScreen = () => {
+    return (
+      <div className="Screen">
+        <h3>Who are you?</h3>
+        <div className="ChoosePlayerControls">
+          <button className="Button" onClick={() => setUser('agent')}>Agent</button>
+          <button className="Button" onClick={() => setUser('spymaster')}>Spymaster</button>
+        </div>
+      </div>
+    ); 
+  }
+  return (
+    <div>
+      {user ? screen[user] : <ChoosePlayerScreen />}
+    </div>
+  )
 }
 
 export default App;
