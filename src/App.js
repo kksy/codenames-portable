cd987aaf5ee57fadd1bccadbf25656a93be31c8e
Kylie Sy <kylksy@gmail.com>
2019-04-28 22:13:42 +1000

    Use firestore for real time get and set, Add new game button for resetting game

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import isEqual from "lodash.isequal";
 import AgentScreen from "./AgentScreen";
 import SpymasterScreen from "./SpymasterScreen";
 import ChoosePlayerScreen from "./ChoosePlayerScreen";
@@ -7,19 +10,101 @@ import { chooseRandomWords } from "./wordList";
 import { generateAgentList } from "./agentList";
 import "./App.css";
 
-const App = () => {
-  const words = chooseRandomWords();
-  const agents = generateAgentList();
-  const initialBoard = generateBoard({ words, agents });
-  const [user, setUser] = useState(null);
-  const [board, updateBoard] = useState(initialBoard);
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+firebase.initializeApp(firebaseConfig);
 
-  const screen = {
-    agent: <AgentScreen board={board} updateBoard={updateBoard} />,
-    spymaster: <SpymasterScreen board={board} updateBoard={updateBoard} />,
+const db = firebase.firestore();
+// const CreateGameSessionScreen = ({ setSession }) => {
+//   return (
+//     <div className="Screen">
+//       <h3>Enter your game pin</h3>
+//         <form>
+//           <button className="Button" onClick={() => setSession({ pin: 'apple' })}>
+//             Join
+//           </button>
+//         </form>
+//     </div>
+//   );
+// }
+
+const NewGameButton = ({ onClick }) => {
+  return (
+    <button className="Button" onClick={onClick}>
+      New Game
+    </button>
+  );
+};
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  const [board, updateBoard] = useState(null);
+  const prevBoardRef = useRef();
+
+  const createNewBoard = () => {
+    const words = chooseRandomWords();
+    const agents = generateAgentList();
+    const initialBoard = generateBoard({ words, agents });
+    db.collection("sessions")
+      .doc("session001")
+      .set(
+        {
+          board: initialBoard,
+        },
+        { merge: true }
+      )
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch(error => {
+        console.error("Error writing document: ", error);
+      });
   };
 
-  return <div>{user ? screen[user] : <ChoosePlayerScreen setUser={setUser} />}</div>;
+  const storeBoardChanges = newBoard => {
+    db.collection("sessions")
+      .doc("session001")
+      .set(
+        {
+          board: newBoard,
+        },
+        { merge: true }
+      )
+      .then(() => {
+        console.log("Document successfully written!");
+      })
+      .catch(error => {
+        console.error("Error writing document: ", error);
+      });
+  };
+
+  useEffect(() => {
+    prevBoardRef.current = board;
+    const docRef = db.collection("sessions").doc("session001");
+    docRef.onSnapshot(snapshot => {
+      const newBoard = snapshot.data().board;
+      if (prevBoardRef && !isEqual(prevBoardRef.current, newBoard)) {
+        updateBoard(snapshot.data().board);
+      }
+    });
+  });
+
+  const screen = {
+    agent: <AgentScreen board={board} updateBoard={storeBoardChanges} />,
+    spymaster: (
+      <div>
+        <SpymasterScreen board={board} />
+        <div className="AgentControls">
+          <NewGameButton onClick={createNewBoard} />
+        </div>
+      </div>
+    ),
+  };
+
+  return <div>{user && board ? screen[user] : <ChoosePlayerScreen setUser={setUser} />}</div>;
 };
 
 export default App;
